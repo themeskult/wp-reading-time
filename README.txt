412a7b7f0e04f9c9e0326e59f91ff825c611db44
Ricardo Rauch <ricardo@gravityonmars.com>
2013-03-21 20:18:36 -0300

    bunch of fixes

--- README.txt
+++ README.txt
@@ -6,11 +6,11 @@ Requires at least: 3.3.1
 Tested up to: 3.3.1
 Stable tag: 1.0
 
-Simple plugin that calculates the remaining reading time as you scroll down. 
+It's a simple plugin that calculates the remaining reading time as you scroll down. 
 
 == Description ==
 
-Simple plugin that calculates the remaining reading time as you scroll down. In order for it to work, all it takes is activating it, and the plugin will run without further ado. Also, this plugin operates over the post, rather than over the website.
+It's a simple plugin that calculates the remaining reading time as you scroll down. In order for it to work, all it takes is activating it, and the plugin will run without further ado. Also, this plugin operates over the post, rather than over the website.
 
 == Installation ==
 
@@ -18,7 +18,7 @@ This section describes how to install the plugin and get it working.
 
 e.g.
 
-1. Upload `wp-reading-time` folder to the `/wp-content/plugins/` directory
+1. Upload `wp-reading-time.php` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Done!
 
--- wp-reading-time.php
+++ wp-reading-time.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: TODO
-Plugin URI: TODO
-Description: TODO
+Plugin Name: WP Reading Time
+Plugin URI: https://github.com/themeskult/wp-reading-time
+Description: It's a simple plugin that calculates the remaining reading time as you scroll down.
 Version: 1.0
-Author: TODO
-Author URI: TODO
-Author Email: TODO
+Author: Ricardo Rauch
+Author URI: http://themeskult.com
+Author Email: store@themeskult.com
 License:
 
-  Copyright 2013 TODO (email@domain.com)
+  Copyright 2013 WP Reading Time (store@themeskult.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as 
@@ -26,8 +26,7 @@ License:
   
 */
 
-// TODO: rename this class to a proper name for your plugin
-class PluginName {
+class WPReadingTime {
 	 
 	/*--------------------------------------------*
 	 * Constructor
@@ -38,107 +37,16 @@ class PluginName {
 	 */
 	function __construct() {
 		
-		// Load plugin text domain
-		add_action( 'init', array( $this, 'plugin_textdomain' ) );
-
-		// Register admin styles and scripts
-		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
-	
 		// Register site styles and scripts
-		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
-	
+		add_action('wp_head', 'my_custom_js');
+
 		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
-		
-	    /*
-	     * TODO:
-	     * Define the custom functionality for your plugin. The first parameter of the
-	     * add_action/add_filter calls are the hooks into which your code should fire.
-	     *
-	     * The second parameter is the function name located within this class. See the stubs
-	     * later in the file.
-	     *
-	     * For more information: 
-	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-	     */
-	    add_action( 'TODO', array( $this, 'action_method_name' ) );
-	    add_filter( 'TODO', array( $this, 'filter_method_name' ) );
 
 	} // end constructor
-	
-	/**
-	 * Fired when the plugin is activated.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
-	 */
-	public function activate( $network_wide ) {
-		// TODO:	Define activation functionality here
-	} // end activate
-	
-	/**
-	 * Fired when the plugin is deactivated.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
-	 */
-	public function deactivate( $network_wide ) {
-		// TODO:	Define deactivation functionality here		
-	} // end deactivate
-	
-	/**
-	 * Fired when the plugin is uninstalled.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
-	 */
-	public function uninstall( $network_wide ) {
-		// TODO:	Define uninstall functionality here		
-	} // end uninstall
-
-	/**
-	 * Loads the plugin text domain for translation
-	 */
-	public function plugin_textdomain() {
-	
-		// TODO: replace "plugin-name-locale" with a unique value for your plugin
-		$domain = 'plugin-name-locale';
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
-        load_textdomain( $domain, WP_LANG_DIR.'/'.$domain.'/'.$domain.'-'.$locale.'.mo' );
-        load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
-
-	} // end plugin_textdomain
-
-	/**
-	 * Registers and enqueues admin-specific styles.
-	 */
-	public function register_admin_styles() {
-	
-		// TODO:	Change 'plugin-name' to the name of your plugin
-		wp_enqueue_style( 'plugin-name-admin-styles', plugins_url( 'plugin-name/css/admin.css' ) );
-	
-	} // end register_admin_styles
-
-	/**
-	 * Registers and enqueues admin-specific JavaScript.
-	 */	
-	public function register_admin_scripts() {
-	
-		// TODO:	Change 'plugin-name' to the name of your plugin
-		wp_enqueue_script( 'plugin-name-admin-script', plugins_url( 'plugin-name/js/admin.js' ) );
-	
-	} // end register_admin_scripts
-	
-	/**
-	 * Registers and enqueues plugin-specific styles.
-	 */
-	public function register_plugin_styles() {
-	
-		// TODO:	Change 'plugin-name' to the name of your plugin
-		wp_enqueue_style( 'plugin-name-plugin-styles', plugins_url( 'plugin-name/css/display.css' ) );
-	
-	} // end register_plugin_styles
 	
 	/**
 	 * Registers and enqueues plugin-specific scripts.
@@ -146,39 +54,21 @@ class PluginName {
 	public function register_plugin_scripts() {
 	
 		// TODO:	Change 'plugin-name' to the name of your plugin
-		wp_enqueue_script( 'plugin-name-plugin-script', plugins_url( 'plugin-name/js/display.js' ) );
+		wp_enqueue_script( 'jquery-reading-time', "https://raw.github.com/themeskult/jquery-reading-time/master/jquery.readingTime.js", array('jquery'));
 	
 	} // end register_plugin_scripts
-	
-	/*--------------------------------------------*
-	 * Core Functions
-	 *---------------------------------------------*/
-	
-	/**
- 	 * NOTE:  Actions are points in the execution of a page or process
-	 *        lifecycle that WordPress fires.
-	 *
-	 *		  WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
-	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
-	 *
-	 */
-	function action_method_name() {
-    	// TODO:	Define your action method here
-	} // end action_method_name
-	
-	/**
-	 * NOTE:  Filters are points of execution in which WordPress modifies data
-	 *        before saving it or sending it to the browser.
-	 *
-	 *		  WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
-	 *		  Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
-	 *
-	 */
-	function filter_method_name() {
-	    // TODO:	Define your filter method here
-	} // end filter_method_name
-  
+
+
+	function my_custom_js() {
+		echo "
+
+(function($) {
+    $('.post').readingTime();
+})( jQuery );
+
+		";
+	}  
 } // end class
 
-// TODO:	Update the instantiation call of your plugin to the name given at the class definition
-$plugin_name = new PluginName();
+// WPReadingTime:	Update the instantiation call of your plugin to the name given at the class definition
+$plugin_name = new WPReadingTime();
